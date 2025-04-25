67de057de50f210dd2cb960086ecbed9193b8eb2
L_OSORIO <luis.osorio@telefonica.com>
2025-04-25 10:00:09 -0300

    subo cambios

--- utils/functions.js
+++ utils/functions.js
@@ -1,45 +1,58 @@
 
+function limpieza(cadena) {
+
+  let newCadena = cadena.replace("'", "");
+  newCadena = newCadena.replace("'", "");
+  newCadena = newCadena.replace("}", "");
+  newCadena = newCadena.trimStart();
+  return newCadena;
+}
+
+function calculoSubTotal(cantidadX,cantidadxbulto,precioX) {
+
+  let subTotal = 0;
+  let cxb = Number(cantidadxbulto);
+  let precio  =  Number(precioX);
+  let cantidad = Number(cantidadX);
+  if ( cxb > 0 ) {
+    subTotal = precio / cxb; 
+    subTotal = subTotal * cantidad;
+  }
+  return subTotal; 
+}
+
+
 function completaItem(codigo, cantidad,materias) {
 
     let newIngredientes = null; 
 
-//    console.log("INGREDIENTES - CODIGO:",codigo); 
-    
     for (x=0;x<materias.length;x++) {
       let materialesPlanchados = materias[x].toString(); 
-  //    console.log("MATERIASPLANCHADA:",materialesPlanchados); 
 
       const materialesPlanchadosLimpio = materialesPlanchados.replace(/\n/g, '');
-    //  console.log("MATERIASPLANCHADA2:",materialesPlanchadosLimpio);
 
       let vectorcito = materialesPlanchadosLimpio.split(",");
-    //  console.log("MATERIASPLANCHADA:",vectorcito[1]); 
 
-      //vemos lo que tenemos e
       const contiene = vectorcito[1].includes("codigo");
       if ( contiene ) {
+        
         const numero = vectorcito[1].match(/\d+/)[0];
-      //  console.log("ENCONTRADO!!!:",numero); 
 
         if ( numero === codigo ) {
-        //  console.log("ENCONTRADO x numero!!!:",numero); 
-        //  console.log("ENCONTRADO x codigo!!!:",codigo); 
 
           let vector1 = vectorcito[2].split(':'); 
-          let descripcion = vector1[1];
-        //  console.log("ENCONTRADO x codigo!!!:",descripcion); 
+          let descripcion = limpieza(vector1[1]);
 
           vector1 = vectorcito[3].split(':'); 
-          let precio = vector1[1];
-        //  console.log("ENCONTRADO x precio!!!:",precio); 
+          let precio = limpieza(vector1[1]);
 
           vector1 = vectorcito[4].split(':'); 
-          let cantidadxbulto = vector1[1];
-        //  console.log("ENCONTRADO x cantidadxbulto!!!:",cantidadxbulto); 
+          let cantidadxbulto = limpieza(vector1[1]);
 
           vector1 = vectorcito[5].split(':'); 
-          let unidad = vector1[1];
-        //  console.log("ENCONTRADO x unidad!!!:",unidad); 
+          let unidad = limpieza(vector1[1]);
+
+          subTotal = calculoSubTotal(cantidad,cantidadxbulto,precio);
 
         newIngredientes = 
             {
@@ -48,7 +61,8 @@ function completaItem(codigo, cantidad,materias) {
               cantidadRequerida: cantidad,
               precio: precio,
               cantidadxbulto,
-              unidad
+              unidad,
+              subTotal
             };
 
           return newIngredientes; 
